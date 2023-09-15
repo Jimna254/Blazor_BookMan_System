dd5bd1c5a0226fc3245e1bb45991a20702d295c9
Jimna <jimmynarokian254@gmail.com>
2023-09-15 12:01:08 +0300

    Add project files.

---
+++ Week8 Assesment/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace Week8_Assesment.Models
+{
+	public class Book
+	{
+		public string Title { get; set; } = string.Empty;
+
+		public string Description { get; set; }= string.Empty;
+		public List<String>? Authors { get; set; }
+	}
+}
